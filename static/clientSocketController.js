8615cdf7eb0ad3e4f1efc6b737d5b5d57ce8d339
Cory Williams <cjwillia@andrew.cmu.edu>
2014-09-02 16:04:45 -0400

    bugfix in client controller

--- static/clientSocketController.js
+++ static/clientSocketController.js
@@ -4,25 +4,34 @@ function socketController(s) {
 };
 
 socketController.prototype.sendButtonDown = function() {
-	if(!this.buttonDown)
+	if(!this.buttonDown) {
 		this.socket.emit('buttondown');
+		return true;
+	}
 	else return false;
 };
 
 socketController.prototype.sendButtonUp = function() {
-	if(this.buttonDown)
+	if(this.buttonDown) {
 		this.socket.emit('buttonup');
+		return true;
+	}
 	else return false;
 };
 
 socketController.prototype.pressButton = function() {
-	if(this.sendButtonDown())
+	if(this.sendButtonDown()) {
 		this.buttonDown = true;
+	}
 	else return false;
 };
 
 socketController.prototype.depressButton = function() {
 	if(this.sendButtonUp())
 		this.buttonDown = false;
-	else return false;
+	else
+	{
+		alert(this.buttonDown +"");
+		return false;
+	}
 };
